0599bb92bde1dccc2cedc4a3102163790f01cc5b
Krockseed <66515313+Krockseed@users.noreply.github.com>
2024-03-08 15:27:29 +0900

    init commit

---
+++ src/main/java/com/example/ec2bbs/Ec2BbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec2bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2BbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ec2BbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ec2bbs/Ec2BbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec2bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2BbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
